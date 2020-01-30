e607de283305fcc3375a2b5fcae6ed00b19ead29
Varun Sharma <varun.sharma@cern.ch>
2020-01-30 09:06:07 -0600

    cleaned master branch

--- SyncExercise/DoubleEleCR/runMC.sh
+++
@@ -1,1 +0,0 @@
-./analyze /hdfs/store/user/varuns/NTuples/MC/MC2018_RunIIAutumn18_102X_JECv19/ST/ST_tW_top_5f/ post_ST_test.root -1 10000 1-1
--- monoJetTools2018/config/__init__.py
+++
@@ -1,8 +0,0 @@
-from ROOT import TFile
-import os
-from mcinfo import *
-from cfg_saveplot import *
-from filevariants import *
-
-proc_path = os.path.dirname(__file__)
-data_pileup = TFile.Open("%s/dataPileup.root" % proc_path)
