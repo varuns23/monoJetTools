ac6dea98a8e87aba7f0e92f79e177ad8fcd759e8
ekoenig4 <ekoenig4@wisc.edu>
2020-01-30 15:04:28 -0600

    fixed 2017 WE submit

--- 2017/SingleEleCR/submit.py
+++ 2017/SingleEleCR/submit.py
@@ -7,12 +7,11 @@ sys.path.append(repo)
 
 from CondorTools.SubmitDataset import submit,options,mclist
 options['year'] = '2017'
-options['region'] = 'WM'
+options['region'] = 'WE'
 options['parallel'] = True
 options['batchsize'] = 10
 # options['submit'] = False
 #----Submit---#
-submit('met',sub='B',label='SingleMu_',filelist=True)
-# submit('met',label='DoubleMu_',filelist=True)
-# for mc in mclist: submit(mc)
+submit('singleele',label='SingleEle_',filelist=True)
+for mc in mclist: submit(mc)
 
