fd2492ad968a52ef8c8c215ae7ee435af3b7e0dd
ekoenig4 <ekoenig4@wisc.edu>
2020-02-18 14:04:59 -0600

    added script for pulling output distributions

---
+++ PlotTool/compareOutput.py
@@ -0,0 +1,60 @@
+import sys
+sys.path.append("PlotTool")
+from ROOT import *
+from PlotTool import *
+import config
+
+gROOT.SetBatch(1)
+
+def compareOutput(fname1,fname2):
+    tfile1 = TFile.Open(fname1)
+    tfile2 = TFile.Open(fname2)
+
+    def compare(hsname):
+        hs1 = tfile1.Get(hsname)
+        hs2 = tfile2.Get(hsname)
+
+        DataStyle(hs1)
+        hs2.SetLineColor(kRed)
+        hs2.SetLineWidth(2)
+
+        c = TCanvas(hsname,hsname,800,800)
+        c.SetLogy()
+        gStyle.SetOptStat(0)
+        gStyle.SetLegendBorderSize(0)
+        c.SetTicks(0,1)
+
+        
+        ratio = TRatioPlot(hs1,hs2)
+        ratio.SetH1DrawOpt("pex0")
+        ratio.SetH2DrawOpt("histsame")
+        ratio.SetGraphDrawOpt("p")
+        ratio.Draw()
+        hi = ratio.GetUpperRefObject()
+        hi.GetYaxis().SetTitle("Events")
+
+        upad = ratio.GetUpperPad()
+        upad.cd()
+        leg = getLegend(xmin=0.5,xmax=0.7)
+        leg.AddEntry(hs1,"%s BU" % hsname,"pl")
+        leg.AddEntry(hs2,"%s UW" % hsname,"fl")
+        leg.Draw()
+
+        lo = ratio.GetLowerRefGraph()
+        lo.GetYaxis().SetTitle("BU/UW")
+        lo.GetYaxis().SetRangeUser(0.75,1.35)
+
+        lo.SetMarkerStyle(20)
+        lo.SetMarkerSize(1)
+        outpath = "/afs/hep.wisc.edu/home/ekoenig4/public_html/MonoJet/Plots2017/BU_Comparison"
+        c.SaveAs("%s/%s.png" % (outpath,hsname))
+        return 
+    keylist1 =[ key.GetName() for key in  tfile1.GetListOfKeys()]
+    keylist2 =[ key.GetName() for key in  tfile2.GetListOfKeys()]
+    for key in keylist1:
+        if key in keylist2:
+            compare(key)
+
+
+compareOutput(sys.argv[1],sys.argv[2])
+    
---
+++ PlotTool/store_variable.py
@@ -0,0 +1,38 @@
+from ROOT import *
+from PlotTool import *
+import config
+import os
+
+if not os.path.isdir('Output/'): os.mkdir('Output/')
+def store(variable):
+
+    output = TFile("Output/monojet_%s_%s.root" % (variable,config.version),"RECREATE")
+    cwd = os.getcwd()
+    regionmap = {
+        "SingleEleCR":"Wen",
+        "SingleMuCR":"Wmn",
+        "DoubleEleCR":"Zee",
+        "DoubleMuCR":"Zmm",
+        "GammaCR":"gjets"
+    }
+    mcmap = {
+        "TTJets":"top",
+        "DYJets":"zll"
+    }
+    def store_region(region):
+        label = regionmap[region]
+        samples = Region(path=region,autovar=True)
+        samples.initiate(variable)
+
+        mcorder = filter(lambda mc:samples[mc].scaled_total/samples.total_bkg > 0.001,samples.MCOrder)
+
+        output.cd()
+        samples["Data"].histo.Clone("%s_data" % label).Write()
+        for mc in mcorder:
+            if mc not in mcmap:mclabel = mc.lower()
+            else: mclabel = mcmap[mc]
+            samples[mc].histo.Clone("%s_%s" % (label,mclabel.lower())).Write()
+    for region in regionmap: store_region(region)
+
+args = parser.parse_args()
+for variable in args.argv: store(variable)
