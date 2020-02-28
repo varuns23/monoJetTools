325552b54afc5a7ec76df5abb846e36f624f715a
ekoenig4 <ekoenig4@wisc.edu>
2020-02-27 18:00:11 -0600

    updated SignalRegion 2018

--- 2018/SignalRegion/src/monoJetClass.C
+++ 2018/SignalRegion/src/monoJetClass.C
@@ -36,6 +36,10 @@ void monoJetClass::Loop(Long64_t maxEvents, int reportEvery) {
     if (ientry < 0) break;
     nb = fChain->GetEntry(jentry);   nbytes += nb;
 
+    if (jentry%reportEvery == 0){
+      cout<<"Analyzing entry "<<jentry<<"/"<<(nentriesToCheck)<<endl;
+    }
+    
     initVars();
 
     float event_weight = 1.;
@@ -48,64 +52,53 @@ void monoJetClass::Loop(Long64_t maxEvents, int reportEvery) {
     }
 
     fillEvent(0,genWeight);
-    if (getMetTrigger() && inclusiveCut()) {
-      fillEvent(1,event_weight);
 
-      if (getMetFilter()) {
-	fillEvent(2,event_weight);
-
-	jetCand = getJetCand();
-	setJetCand(jetCand);
-	if (jetCand.size() > 0) {
-	  fillEvent(3,event_weight);
-
-	  if (electron_veto_looseID(jetindex).size() == 0) {
-	    fillEvent(4,event_weight);
-
-	    if (muon_veto_looseID(jetindex).size() == 0) {
-	      fillEvent(5,event_weight);
-
-	      if (photon_veto_looseID(jetindex).size() == 0) {
-		fillEvent(6,event_weight);
-
-		if (tau_veto_looseID(jetindex).size() == 0) {
-		  fillEvent(7,event_weight);
-
-		  if (bjet_veto_looseID(jetindex).size() == 0) {
-		    fillEvent(8,event_weight);
-
-		    float dpfcalo = fabs(pfMET-caloMET)/pfMET;
-		    h_metcut->Fill(dpfcalo,event_weight);
-		    if (dpfcalo < metRatioCut) {
-		      fillEvent(9,event_weight);
-
-		      vector<int> jetlist = jet_veto_looseID(jetindex);
-		      float mindPhiJetMET = dPhiJetMETmin(jetlist,pfMETPhi);
-		      h_dphimin->Fill(mindPhiJetMET,event_weight);
-		      if (mindPhiJetMET > dPhiJetMETCut) {
-			fillEvent(10,event_weight);
-
-			if (pfMET > recoilCut) {
-			  fillEvent(11,event_weight);
-
-			  if (getEleHEMVeto()) {
-			    fillEvent(12,event_weight);
-			  }
-			}
-		      }
-		    }
-		  }
-		}
-	      }
-	    }
-	  }
-	}
-      }
+    if (!getMetTrigger()) continue;
+    if (!inclusiveCut()) continue;
+    if (isMC) {
+      ApplyMET_TriggerSF(event_weight);
     }
+    fillEvent(1,event_weight);
 
-    if (jentry%reportEvery == 0){
-      cout<<"Finished entry "<<jentry<<"/"<<(nentriesToCheck-1)<<endl;
-    }
+    if (!getMetFilter()) continue;
+    fillEvent(2,event_weight);
+
+    if(!getJetHEMVeto())continue;                                                                                                                             
+    fillEvent(3,event_weight);
+    
+    if (!electron_veto()) continue;
+    fillEvent(4,event_weight);
+
+    if (!muon_veto()) continue;
+    fillEvent(5,event_weight);
+
+    if (!photon_veto()) continue;
+    fillEvent(6,event_weight);
+
+    if (!tau_veto()) continue;
+    fillEvent(7,event_weight);
+
+    if (!bjet_veto(bjetDeepCSVCut_2017)) continue;
+    fillEvent(8,event_weight);
+    
+    vector<int> jetlist = getLooseJet();
+    float mindPhiJetMET = dPhiJetMETmin(jetlist,pfMETPhi);
+    if (pfMET > recoilCut) h_dphimin->Fill(mindPhiJetMET,event_weight);
+    if (mindPhiJetMET <= dPhiJetMETCut) continue;
+    fillEvent(9,event_weight);
+		      
+    float dpfcalo = fabs(pfMET-caloMET)/pfMET;
+    if (pfMET > recoilCut) h_metcut->Fill(dpfcalo,event_weight);
+    if (dpfcalo >= metRatioCut) continue;
+    fillEvent(10,event_weight);
+
+    if (pfMET <= recoilCut) continue;
+    fillEvent(11,event_weight);
+
+    jetCand = getJetCand(jetlist);
+    if (jetCand.size() < 1) continue;
+    setJetCand(jetCand);
+    fillEvent(12,event_weight);
   }
 
 }//Closing the Loop function
@@ -115,8 +108,8 @@ void monoJetClass::BookHistos(const char* outputFilename) {
   output = new TFile(outputFilename, "RECREATE");
   output->cd();
 
-  cutflow = new Cutflow({"Total Events","Triggers","MET Filters","Jet Selection"
-	,"Electron Veto","Muon Veto","Photon Veto","Tau Veto","BJet Veto","dPFCaloMET","minDPhiJetMET","pfMET250","EleHEMVeto"});
+  cutflow = new Cutflow(this,{"Total Events","Trigger","MET Filters","HEM Veto","Electron Veto","Muon Veto"
+	,"Photon Veto","Tau Veto","BJet Veto","minDPhiJetMET","dPFCaloMET","Recoil","Jet Selection"});
 
   monoJetYear::BookHistos(-1,"");
   for(int i = 0; i<nHisto; i++) {
