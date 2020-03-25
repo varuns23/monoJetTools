45926ae2fe859a0e345fcba8bc01c75e892a5102
ekoenig4 <ekoenig4@wisc.edu>
2020-03-25 09:31:48 -0500

    fixed comments

--- PlotTool/getANplots.sh
+++ PlotTool/getANplots.sh
@@ -37,12 +37,12 @@ run2() {
 }
 
 region() {
-    # run SignalRegion || exit 1
+    run SignalRegion || exit 1
     run SingleEleCR $singleleps  || exit 1
     run SingleMuCR $singleleps || exit 1
-    # run DoubleEleCR $doubleleps || exit 1
-    # run DoubleMuCR $doubleleps || exit 1
-    # run GammaCR $gamma || exit 1
+    run DoubleEleCR $doubleleps || exit 1
+    run DoubleMuCR $doubleleps || exit 1
+    run GammaCR $gamma || exit 1
 }
 
 region2() {
