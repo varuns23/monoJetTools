8ea0626c65679661898c5ace916977e380dd64c5
ekoenig4 <ekoenig4@wisc.edu>
2020-03-25 13:32:36 -0500

    updated sys file format to match gitlab

--- PlotTool/saveplot.py
+++ PlotTool/saveplot.py
@@ -9,109 +9,43 @@ import config
 gROOT.SetBatch(1)
 
 dirlist = ["SignalRegion","SingleEleCR","SingleMuCR","DoubleEleCR","DoubleMuCR","GammaCR"]
-dirmap = {"SignalRegion":"sr","DoubleEleCR":"ze","DoubleMuCR":"zm","SingleEleCR":"we","SingleMuCR":"wm","GammaCR":"ga"}
+dirmap = {"SignalRegion":"signal","DoubleEleCR":"Zee","DoubleMuCR":"Zmm","SingleEleCR":"Wen","SingleMuCR":"Wmn","GammaCR":"gjets"}
+procmap = {"Data":"data","ZJets":"zjets","WJets":"wjets","DYJets":"zll","GJets":"gjets","TTJets":"top","DiBoson":"diboson","QCD":"qcd"}
+signalmap = {"Axial":"axial"}
 if not path.isdir("Systematics"): mkdir("Systematics")
 
 def validHisto(hs,total=0,threshold=0.2):return hs.Integral() > threshold*total
 def validShape(up,dn):return any( up[ibin] != dn[ibin] for ibin in range(1,up.GetNbinsX()+1) ) and validHisto(up) and validHisto(dn)
 
-tfcorrelation = {
-    "QCD_Scale":True,
-    "QCD_Shape":True,
-    "QCD_Proc":True,
-    "NNLO_Sud":True,
-    "NNLO_Miss":False,
-    "NNLO_EWK":False,
-    "QCD_EWK_Mix":True
-}
-
-def WCR_TF(wln,sr,output):
-    print "-%s to %s Transfer" % (dirmap[wln.region],dirmap[sr.region])
-    tf = Transfer('%s_to_sr' % dirmap[wln.region],sr['WJets'],wln['WJets'],['sr',dirmap[wln.region]])
-    output.cd(); output.cd(dirmap[wln.region]);
-    cwd = gDirectory.mkdir("transfer"); cwd.cd()
-    tf.histo.Write('%s_to_sr' % dirmap[wln.region])
-def ZCR_TF(zll,sr,output):
-    print "-%s to %s Transfer" % (dirmap[zll.region],dirmap[sr.region])
-    tf = Transfer("%s_to_sr" % dirmap[zll.region],sr["ZJets"],zll["DYJets"],['sr',dirmap[zll.region]])
-    output.cd(); output.cd(dirmap[zll.region])
-    cwd = gDirectory.mkdir("transfer"); cwd.cd()
-    tf.histo.Write('%s_to_sr' % dirmap[zll.region])
-def GCR_TF(ga,sr,output):
-    print "-%s to %s Transfer" % (dirmap[ga.region],dirmap[sr.region])
-    tf = Transfer("%s_to_sr" % dirmap[ga.region],sr["ZJets"],ga["GJets"],['sr',dirmap[ga.region]])
-    output.cd(); output.cd(dirmap[ga.region])
-    cwd = gDirectory.mkdir("transfer"); cwd.cd()
-    tf.histo.Write('%s_to_sr' % dirmap[ga.region])
-    tfname = "ga_to_sr_%s"
-    for nuisance,isCorrelated in tfcorrelation.iteritems():
-        tf.addUnc(nuisance,isCorrelated)
-        if isCorrelated:
-            uncname = tfname % nuisance
-            print "--Writing %s" % nuisance
-            up,dn = tf.nuisances[nuisance].GetHistos()
-            up.Write(uncname+"Up")
-            dn.Write(uncname+"Down")
-        else:
-            for proc in ('sr',dirmap[ga.region]):
-                nuisance_proc = "%s_%s" % (nuisance,proc)
-                print "--Writing %s" % nuisance_proc
-                uncname = tfname % nuisance_proc
-                up,dn = tf.nuisances[nuisance_proc].GetHistos()
-                up.Write(uncname+"Up")
-                dn.Write(uncname+"Down")
-    return
-def SR_TF(sr,output):
-    print "-Wsr to Zsr Transfer"
-    tf = Transfer("wsr_to_zsr",sr["ZJets"],sr["WJets"],['zsr','wsr'])
-    output.cd(); output.cd(dirmap[sr.region])
-    cwd = gDirectory.mkdir("transfer"); cwd.cd()
-    tf.histo.Write('wsr_to_zsr')
-    tfname = "wsr_to_zsr_%s"
-    for nuisance,isCorrelated in tfcorrelation.iteritems():
-        tf.addUnc(nuisance,isCorrelated)
-        cwd.cd()
-        if isCorrelated:
-            uncname = tfname % nuisance
-            print "--Writing %s" % nuisance
-            up,dn = tf.nuisances[nuisance].GetHistos()
-            up.Write(uncname+"Up")
-            dn.Write(uncname+"Down")
-        else:
-            for proc in ('zsr','wsr'):
-                nuisance_proc = "%s_%s" % (nuisance,proc)
-                print "--Writing %s" % nuisance_proc
-                uncname = tfname % nuisance_proc
-                up,dn = tf.nuisances[nuisance_proc].GetHistos()
-                up.Write(uncname+"Up")
-                dn.Write(uncname+"Down")
 def SaveRegion(region,save):
     region = Region(path=region,show=False,autovar=True)
     region.initiate(variable)
-    region.setSumOfBkg()
     
     if save.tfile is None: save.tfile = TFile("Systematics/%s_%s.sys.root" % (region.varname,region.year),'recreate')
     output = save.tfile
     
     print "Writing Histograms from %s" % region.region
-    output.cd(); cwd = output.mkdir(dirmap[region.region]); cwd.cd()
-    region['SumOfBkg'].histo.Write("SumOfBkg")
+    output.cd();
+    # region['SumOfBkg'].histo.Write("SumOfBkg")
     if region.isBlinded:
-        region['Data'].histo.Write('data_obs')
+        region['Data'].histo.Write("%s_data" % dirmap[region.region])
     for process in region:
         print "--Writing %s Histogram" % process.name
-        if process.proctype == "data": process.histo.Write("data_obs")
-        else:
-            process.histo.Write(process.process)
-            for nuisance in region.variable.nuisances:
-                process.addUnc(nuisance)
-                cwd.cd()
-                if nuisance in process.nuisances:
-                    up,dn = process.nuisances[nuisance].GetHistos()
-                    if not validShape(up,dn): continue
-                    print "----Writing",process.nuisances[nuisance]
-                    up.Write("%s_%sUp"%(process.process,nuisance))
-                    dn.Write("%s_%sDown"%(process.process,nuisance))
+        
+        if process.proctype == "signal":
+            sigproc = next( signalmap[signal] for signal in signalmap if signal in process.process )
+            export = "%s_%s" % (dirmap[region.region],sigproc)
+        else: export = "%s_%s" % (dirmap[region.region],procmap[process.process])
+        process.histo.Write(export)
+            # for nuisance in region.variable.nuisances:
+            #     process.addUnc(nuisance)
+            #     cwd.cd()
+            #     if nuisance in process.nuisances:
+            #         up,dn = process.nuisances[nuisance].GetHistos()
+            #         if not validShape(up,dn): continue
+            #         print "----Writing",process.nuisances[nuisance]
+            #         up.Write("%s_%sUp"%(process.process,nuisance))
+            #         dn.Write("%s_%sDown"%(process.process,nuisance))
     return region
 def SavePlot(variable):
     print variable
@@ -119,24 +53,6 @@ def SavePlot(variable):
     class save: pass
     save.tfile = None
     regionmap = { dirmap[region]:SaveRegion(region,save) for region in dirlist }
-    output = save.tfile
-    SR_TF(regionmap["sr"],output)
-    for wln in ("we","wm"): WCR_TF(regionmap[wln],regionmap["sr"],output)
-    for zll in ("ze","zm"): ZCR_TF(regionmap[zll],regionmap["sr"],output)
-    GCR_TF(regionmap["ga"],regionmap["sr"],output)
-    output.cd()
-    norm = regionmap["sr"]
-    lumi_hs = TH1F("lumi","lumi",1,0,1)
-    lumi_hs.SetBinContent(1,norm.lumi)
-    lumi_hs.Write()
-    year_hs = TH1F("year","year",1,0,1)
-    year_hs.SetBinContent(1,int(norm.year))
-    year_hs.Write()
-    var_hs = TH1F("variable",variable+';'+norm.name,1,0,1)
-    var_hs = norm.variable.template.Clone('variable')
-    var_hs.Reset(); var_hs.SetTitle(variable); var_hs.GetXaxis().SetTitle(norm.name);
-    var_hs.Write()
-    output.Close()
 ################################################################################
 if __name__ == "__main__":
     from PlotTool import parser
