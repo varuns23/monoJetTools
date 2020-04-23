183fe067e81c1197e2111e5fc485001183d106b1
ekoenig4 <ekoenig4@wisc.edu>
2020-04-23 14:09:13 -0500

    added submit files for lepveto

---
+++ 2017/LeptonVeto/submit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import os
+import sys
+cmssw = os.getenv("CMSSW_BASE")
+repo = '%s/src/monoJetTools/' % cmssw
+sys.path.append(repo)
+
+from CondorTools.SubmitDataset import submit,options,mclist,signalist
+options['year'] = '2017'
+options['region'] = 'SR'
+options['parallel'] = False
+options['batchsize'] = 150
+# options['submit'] = False
+#----Submit---#
+# submit('met',sub='B',filelist=True)
+# for signal in signalist: submit(signal,split=1)
+# for mc in mclist: submit(mc)
+submit("wjets")
