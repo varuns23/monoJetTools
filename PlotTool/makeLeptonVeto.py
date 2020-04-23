4cca8e036899e3d1177eaee6c50c1de2050c8321
ekoenig4 <ekoenig4@wisc.edu>
2020-04-23 12:30:07 -0500

    updated lepveto script

--- PlotTool/makeLeptonVeto.py
+++ PlotTool/makeLeptonVeto.py
@@ -9,7 +9,7 @@ import os
 from PlotTool import parser
 parser.parse_args()
 
-config.mcinfo = ["WJets","QCD"]
+config.mclist = ["WJets","QCD"]
 
 gROOT.SetBatch(1)
 
@@ -25,7 +25,7 @@ def plotLeptonVeto(nominal,lepveto):
     def ProcessVeto(process):
         nom = nominal[process].histo
         veto = lepveto[process].histo
-        eff=GetRatio(veto,nom)
+        sf=GetRatio(veto,nom)
         
         c = TCanvas("c", "canvas",800,800);
         gStyle.SetOptStat(0);
@@ -34,22 +34,41 @@ def plotLeptonVeto(nominal,lepveto):
         # c.SetLogy();
         # c.cd();
         
-        pad1 = TPad("pad1","pad1",0.02,0.02,0.99,0.99);
+        pad1 = TPad("pad1","pad1",0.01,0.25,0.99,0.99);
         pad1.Draw(); pad1.cd();
-        # pad1.SetLogy();
+        pad1.SetLogy();
         pad1.SetFillColor(0); pad1.SetFrameBorderMode(0); pad1.SetBorderMode(0);
-        # pad1.SetBottomMargin(0.35);
+        pad1.SetBottomMargin(0);
 
-        DataStyle(eff)
-        eff.GetYaxis().SetTitle('%s Veto' % lepveto.lep)
-        eff.GetYaxis().SetRangeUser(0,2)
-        eff.Draw('lpex0')
+        DataStyle(nom)
+        MCStyle(veto,kRed-7)
+
+        veto.Draw("hist same")
+        nom.Draw("pex0 same")
+
+        leg = getLegend()
+        leg.AddEntry(nom,"Nominal Hard Veto","pl")
+        leg.AddEntry(veto,"%s Veto SF"%lepveto.lep,"f")
+        leg.Draw()
         
         lumi_label = '%s' % float('%.3g' % (nominal.lumi/1000.)) + " fb^{-1}"
         if (parser.args.normalize): lumi_label="Normalized"
-        texLumi,texCMS = getCMSText(lumi_label,nominal.year,scale=0.8)
+        texLumi,texCMS = getCMSText(lumi_label,nominal.year)
         texLumi.Draw();
         texCMS.Draw();
+
+        c.cd()
+        pad2 = TPad("pad2","pad2",0.01,0.01,0.99,0.25)
+        pad2.Draw(); pad2.cd();
+        pad2.SetFillColor(0); pad2.SetFrameBorderMode(0); pad2.SetBorderMode(0);
+        pad2.SetTopMargin(0.)
+        pad2.SetBottomMargin(0.35);
+        pad2.SetGridy()
+
+        RatioStyle(sf,rymin=0,rymax=2,xname=nominal.variable.xaxis_title,yname="SF/Hard")
+        sf.Draw("pex0")
+        
+        
         SaveAs(c,"%s_%s"%(lepveto.lep,lepveto.variable.base),year=nominal.year,sub="LeptonVeto/%s"%process)
     for process in ("WJets","QCD"): ProcessVeto(process)
 #-- Electron Veto --#
