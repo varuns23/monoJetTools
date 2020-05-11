6035e6ed4ef34ee4fce3be3d9e4802b236262210
ekoenig4 <ekoenig4@wisc.edu>
2020-05-11 11:02:08 -0500

    always scale by bin width

--- PlotTool/PlotTool/VariableInfo.py
+++ PlotTool/PlotTool/VariableInfo.py
@@ -130,9 +130,9 @@ class VariableInfo:
         self.xaxis_title = self.template.GetXaxis().GetTitle()
         self.yaxis_title = self.template.GetYaxis().GetTitle()
 
-        self.scaleWidth = False
-        if not parser.args.no_width and self.template.ClassName() == "TH1":
-            self.scaleWidth = any( "%.3f" % self.template.GetBinWidth(ibin) != "%.3f" % self.template.GetBinWidth(ibin+1) for ibin in range(1,self.template.GetNbinsX()) )
+        self.scaleWidth = True
+        # if not parser.args.no_width and self.template.ClassName() == "TH1":
+            # self.scaleWidth = any( "%.3f" % self.template.GetBinWidth(ibin) != "%.3f" % self.template.GetBinWidth(ibin+1) for ibin in range(1,self.template.GetNbinsX()) )
         
     def initGlobal(self,tfile,variable):
         self.isGlobal = True
